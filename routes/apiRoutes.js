dcf6bf8f2c8fff4bccb58076e8c3ab9c7c3d111f
Kazzpars <achmadridwan587@gmail.com>
2026-06-16 08:03:37 +0700

    perbaikan halaman webgis supaya bisa diakses oleh user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
+
 const reportController = require('../controllers/reportController');
 const uploadMiddleware = require('../middlewares/uploadMiddleware');
 const verifyToken = require('../middlewares/authMiddleware');
 const authController = require('../controllers/authController');
-// Endpoint API
+
+// Publik
 router.get('/reports', reportController.getReportsAPI);
 router.post('/reports', uploadMiddleware.single('foto'), reportController.createReportAPI);
-router.put('/reports/:id/status', reportController.updateStatusAPI);
+
+// Admin
+router.put('/reports/:id/status', verifyToken, reportController.updateStatusAPI);
+
+// Login admin
 router.post('/login', authController.loginAdmin);
 
-
 module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+// const reportController = require('../controllers/reportController');
+// const uploadMiddleware = require('../middlewares/uploadMiddleware');
+// const verifyToken = require('../middlewares/authMiddleware');
+// const authController = require('../controllers/authController');
+// // Endpoint API
+// router.get('/reports', reportController.getReportsAPI);
+// router.post('/reports', uploadMiddleware.single('foto'), reportController.createReportAPI);
+// router.put('/reports/:id/status', reportController.updateStatusAPI);
+// router.post('/login', authController.loginAdmin);
+
+
+// module.exports = router;
